771fa5def5416a82010bd55533448de6037d584c
Park <jkpark@lineupkorea.com>
2025-06-30 16:37:29 +0900

    250630 PJK

--- user-service/src/main/java/com/example/userservice/controller/UserController.java
+++ user-service/src/main/java/com/example/userservice/controller/UserController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping("/hello")
     public String hello(){
-        return "hello from User Service";
+        return "hello from User Service!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
     }
 }
