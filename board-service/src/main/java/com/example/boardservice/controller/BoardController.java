aea8c0f6953c5c0565e35020a71ca8d56bb4a0ad
Park <jkpark@lineupkorea.com>
2025-07-07 09:26:38 +0900

    250707 PJK

--- board-service/src/main/java/com/example/boardservice/controller/BoardController.java
+++ board-service/src/main/java/com/example/boardservice/controller/BoardController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class BoardController {
     @GetMapping("/hello")
     public String hello(){
-        return "hello from Board Service";
+        return "hello from Board Service~~ come on NOW";
     }
 }
