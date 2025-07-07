2ddcff2e0f083c444e07a92cc8132e7254e8ddc7
jkpark <jongkwong@naver.com>
2025-07-07 14:13:48 +0900

    Board

--- board-service/src/main/java/com/example/boardservice/controller/BoardController.java
+++ board-service/src/main/java/com/example/boardservice/controller/BoardController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class BoardController {
     @GetMapping("/hello")
     public String hello(){
-        return "hello from Board Service~~ come on NOW~~~~~~~~~~~~~~~~!~~~~~~~~~";
+        return "이건 보드 헬로입니다.";
     }
 }
